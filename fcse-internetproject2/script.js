5ee0392adf7bf0496c4a8a62f6514c87ad516174
Molly <mg27756@westridge.org>
2024-04-30 15:01:40 -0700

    randomizer

--- fcse-internetproject2/script.js
+++ fcse-internetproject2/script.js
@@ -5,6 +5,7 @@
  * https://api.thecatapi.com/v1/images/search
  */
 
+
 async function getCat() {
   let response = await fetch("https://api.thecatapi.com/v1/images/search");
   if (response.ok) {
@@ -14,9 +15,12 @@ async function getCat() {
   } else {
     alert("HTTP-Error: " + response.status);
   }
+    let cat = getCat[0].url
 }
+
 
 
 function randomize() {
     let which = Math.floor(Math.random() * 4) + 1;
+    return which;
 } 
